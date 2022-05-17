196e6499008bf8ddbab5155339298f2dbfa45b01
radu <nedelcu.radu21@gmail.com>
2022-05-17 20:42:51 +0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+const presets = ['module:metro-react-native-babel-preset']
+const plugins = []
+
+plugins.push([
+  'module-resolver',
+  {
+    root: ['./src'],
+    extensions: ['.js', '.json'],
+    alias: {
+      '@': './src',
+    },
+  "resolutions": {
+    "@babel/runtime": "7.12.0"
+  },
+}
+])
+
+plugins.push('react-native-reanimated/plugin')
+
+module.exports = {
+  presets,
+  plugins,
+}
---
+++ components/Head/head.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View } from "react-native";
+export default function Head({ position, size }) {
+  return (
+    <View
+      style={{
+        width: size,
+        height: size,
+        backgroundColor: "red",
+        position: "absolute",
+        // left: position[0] * size,
+        // top: position[1] * size,
+        left: 0 * size,
+        top: 0 * size,
+      }}
+    ></View>
+  );
+} 
